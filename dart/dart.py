b8d1a51c591e7cb7ff4d7568dac87135d43c4088
Tianshu <thetianshuhuang@gmail.com>
2022-12-12 11:36:59 -0500

    Removed multi-gpu (probably not ever going to be worth it)

--- dart/dart.py
+++ dart/dart.py
@@ -12,7 +12,6 @@ from jaxtyping import Float32, Array, PyTree
 from beartype.typing import Callable, NamedTuple, Optional
 
 from .sensor import VirtualRadar
-from .sensor_column import TrainingColumn
 
 
 class ModelState(NamedTuple):
@@ -30,17 +29,12 @@ class DART:
     sensor: Sensor model parameters.
     optimizer: Model optax optimizer.
     sigma: Field function generator; should close the actual field function.
-    gpus: Number of gpus to use. If None, uses all.
     """
 
     def __init__(
         self, sensor: VirtualRadar, optimizer, sigma: Callable[
-            [], Callable[[Float32[Array, "3"]], Float32[Array, ""]]],
-        gpus: Optional[int] = None
+            [], Callable[[Float32[Array, "3"]], Float32[Array, ""]]]
     ) -> None:
-
-        self.gpus = gpus if gpus else len(jax.devices())
-        self.devices = jax.local_devices()[:gpus]
 
         def forward(batch):
             key = hk.next_rng_key()
@@ -66,14 +60,13 @@ class DART:
         """Train model."""
         # Note: not putting step in a closure here results in a ~100x
         # performance penalty!
-        def loss_and_grad(state, rng, batch):
+        def step(state, rng, batch):
             def loss_func(params):
                 y_pred = self.model.apply(params, rng, batch)
                 return jnp.sum((batch.data - y_pred)**2) / batch.data.shape[0]
 
-            return jax.value_and_grad(loss_func)(state.params)
+            loss, grads = jax.value_and_grad(loss_func)(state.params)
 
-        def step(grads, state):
             clip = jax.tree_util.tree_map(jnp.nan_to_num, grads)
             updates, opt_state = self.optimizer.update(
                 clip, state.opt_state, state.params)
@@ -82,7 +75,7 @@ class DART:
             params = jax.tree_util.tree_map(
                 partial(jnp.clip, a_min=0.0, a_max=1.0), params)
 
-            return ModelState(params, opt_state)
+            return loss, ModelState(params, opt_state)
 
         for i in range(epochs):
             with tqdm(
@@ -91,29 +84,9 @@ class DART:
                 avg = 0.
                 for j, batch in enumerate(epoch):
                     key, rng = jax.random.split(key, 2)
-
                     batch = jax.tree_util.tree_map(jnp.array, batch)
-                    if self.gpus > 1:
-                        def distribute(x):
-                            shards = [
-                                s for s in x.reshape(
-                                    self.gpus,
-                                    int(batch.data.shape[0] / self.gpus),
-                                    *x.shape[1:])
-                            ]
-                            return jax.device_put_sharded(shards, self.devices)
 
-                        batch = jax.tree_util.tree_map(distribute, batch)
-                        rng = jnp.array(jax.random.split(rng, self.gpus))
-                        loss, grad = jax.pmap(
-                            partial(loss_and_grad, state))(rng, batch)
-                        loss = jnp.mean(loss)
-                        grad = jax.tree_util.tree_map(
-                            lambda x: jnp.mean(x), grad)
-                    else:
-                        loss, grad = jax.jit(loss_and_grad)(state, rng, batch)
-
-                    state = jax.jit(step)(grad, state)
+                    loss, state = jax.jit(step)(state, rng, batch)
                     avg = (avg * j + loss) / (j + 1)
                     epoch.set_postfix(loss=avg)
 
