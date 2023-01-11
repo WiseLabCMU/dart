bd136bcd361eff7974f0e45b1aa723a0f4c021ab
CalculatorSP <mike.handidate@gmail.com>
2023-01-11 08:05:20 -0500

    parameterize framelen

--- matlab/scans_from_file.m
+++ matlab/scans_from_file.m
@@ -2,22 +2,27 @@
 filename = 'E:\dartdata\CupData\cup.mat';
 
 load(filename, 'frames', 'start_time', 'end_time');
-N = size(frames, 1);
 dt_start = datetime(start_time, 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss.SSSSSS''Z''');
 dt_end = datetime(end_time, 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss.SSSSSS''Z''');
+duration = posixtime(dt_end) - posixtime(dt_start);
 
-chirplen = 512;
-framelen = 1024;
-a = squeeze(frames(:,1,1,:));
-a = a(1:(floor(size(a, 1) / framelen)*framelen), :);
+framelen = 256;
+chirplen = size(frames, 4);
+numframes = floor(size(frames, 1) / framelen);
+numsamples = numframes * framelen;
+dt = duration / numframes;
+
+a = squeeze(frames(1:numsamples, 1, 1, :));
 b = reshape(a.', chirplen, framelen, []);
 
-x = -512:511;
-y = 0:511;
-for i = 1:126
+x = 0:framelen-1;
+y = 0:chirplen-1;
+f = waitbar(0, 'Plotting frames');
+for i = 1:numframes
     c = fftshift(fft2(b(:,:,i)), 2);
     plotComplex(x, y, c);
-    pause(0.2);
+    f = waitbar(i/numframes, f, 'Plotting frames');
+    pause(dt);
 end
 
 % end
