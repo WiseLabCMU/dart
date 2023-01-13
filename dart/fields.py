c0c02a175b50b5f9a3f3c88306fb37ae9549b405
Tianshu <thetianshuhuang@gmail.com>
2023-01-13 12:46:00 -0500

    Added NGP, sigma + alpha, other utilities

--- dart/fields.py
+++ dart/fields.py
@@ -1,9 +1,11 @@
 """Reflectance field functions."""
 
-from jaxtyping import Float32, Array
+from functools import partial
+from jaxtyping import Float32, Integer, Array
 from beartype.typing import Union
 
 from jax import numpy as jnp
+import jax
 import haiku as hk
 
 from .spatial import interpolate
@@ -14,7 +16,7 @@ class GroundTruth:
 
     Parameters
     ----------
-    grid: Reflectance grid; is trilinearly interpolated.
+    grid: (reflectance, transmittance) grid; is trilinearly interpolated.
     lower: Lower corner of the grid.
     resolution: Resolution in units per grid cell. Can have the same resolution
         for each axis or different resolutions.
@@ -28,19 +30,24 @@ class GroundTruth:
         self.resolution = resolution
         self.grid = grid
 
-    def __call__(self, x: Float32[Array, "3"]) -> Float32[Array, ""]:
+    def __call__(self, x: Float32[Array, "3"]) -> Float32[Array, "2"]:
         """Index into reflectance map."""
         index = (x - self.lower) * self.resolution
         valid = jnp.all(
             (0 <= index) & (index <= jnp.array(self.grid.shape) - 1))
-        return jnp.where(
-            valid,
-            interpolate(index, self.grid.reshape(*self.grid.shape, 1))[0],
-            jnp.zeros(()))
+        return jnp.where(valid, interpolate(index, self.grid), jnp.zeros((2,)))
 
 
 class SimpleGrid(hk.Module):
-    """Simple reflectance grid."""
+    """Simple reflectance grid.
+
+    Parameters
+    ----------
+    size: Grid size (x, y, z) dimensions.
+    lower: Lower corner of the grid.
+    resolution: Resolution in units per grid cell. Can have the same resolution
+        for each axis or different resolutions.
+    """
 
     def __init__(
         self, size: tuple[int, int, int], lower: Float32[Array, "3"],
@@ -51,20 +58,28 @@ class SimpleGrid(hk.Module):
         self.resolution = resolution
         self.size = size
 
-    def __call__(self, x: Float32[Array, "3"]) -> Float32[Array, ""]:
+    def __call__(self, x: Float32[Array, "3"]) -> Float32[Array, "2"]:
         """Index into learned reflectance map."""
-        grid = hk.get_parameter("grid", self.size, init=jnp.zeros)
+        grid = hk.get_parameter("grid", (*self.size, 2), init=jnp.zeros)
         index = (x - self.lower) * self.resolution
-        valid = jnp.all(
-            (0 <= index) & (index <= jnp.array(self.size) - 1))
-        return jnp.where(
-            valid,
-            interpolate(index, grid.reshape(*grid.shape, 1))[0],
-            jnp.zeros(()))
+        valid = jnp.all((0 <= index) & (index <= jnp.array(self.size) - 1))
+        return jnp.where(valid, interpolate(index, grid), jnp.zeros((2,)))
+
+    @staticmethod
+    def project(params):
+        """Project grid parameters to [0, 1]."""
+        return jax.tree_util.tree_map(
+            partial(jnp.clip, a_min=0.0, a_max=1.0), params)
 
 
-class NGPHashTable:
-    """Single hash table for NGP field.
+class NGP:
+    """NGP field.
+
+    Parameters
+    ----------
+    size: Hash table size (and feature dimension)
+    levels: Resolution of each hash table level. The length determines the
+        number of hash tables.
 
     References
     ----------
@@ -72,12 +87,35 @@ class NGPHashTable:
         Multiresolution Hash Encoding," 2022.
     """
 
-    def __init__(self, size):
+    def __init__(
+            self, levels: Float32[Array, "n"],
+            size: tuple[int, int] = (16384, 2)):
         self.size = size
+        self.levels = levels
+        self.head = hk.Sequential([
+            hk.Linear(32), jax.nn.relu,
+            hk.Linear(2), jax.nn.relu
+        ])
 
-    def hash(self, x):
+    def hash(self, x: Integer[Array, "3"]) -> Integer[Array, ""]:
         """Apply hash function specified by NGP (Eq. 4 [1])."""
-        pi2 = 2654435761
-        pi3 = 805459861
+        x = x.astype(jnp.uint32)
+        pi2 = jnp.array(2654435761, dtype=jnp.uint32)
+        pi3 = jnp.array(805459861, dtype=jnp.uint32)
 
-        return (x[0] + x[1] * pi2 + x[2] * pi3) % self.size
+        return (x[0] + x[1] * pi2 + x[2] * pi3) % self.size[0]
+
+    def __call__(self, x: Float32[Array, "3"]) -> Float32[Array, "2"]:
+        """Index into learned reflectance map."""
+        xscales = x.reshape(1, -1) * self.levels.reshape(-1, 1)
+        grid = hk.get_parameter(
+            "grid", (self.levels.shape[0], *self.size),
+            init=hk.initializers.RandomUniform(0, 0.0001))
+
+        def interpolate_level(xscale, grid_level):
+            def hash_table(c):
+                return grid_level[self.hash(c)]
+            return interpolate(xscale, jax.vmap(hash_table))
+
+        table_out = jax.vmap(interpolate_level)(xscales, grid)
+        return self.head(table_out.reshape(-1))
