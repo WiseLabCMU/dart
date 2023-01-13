09acb708f2975f898e010c975879cdd3939400fb
John <jmiller4@andrew.cmu.edu>
2023-01-13 14:45:47 -0500

    save frames to .mat file

--- matlab/scans_from_file.m
+++ matlab/scans_from_file.m
@@ -11,13 +11,17 @@ end
 
 load(filename, 'frames', 'start_time', 'end_time');
 
-dt_start = datetime(start_time, 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss.SSSSSS''Z''');
-dt_end = datetime(end_time, 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss.SSSSSS''Z''');
-duration = posixtime(dt_end) - posixtime(dt_start);
-
 chirplen = size(frames, 4); % before decmiation
 numframes = floor(size(frames, 1) / framelen);
 numsamples = numframes * framelen;
+
+t_start = posixtime(datetime(start_time, 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss.SSSSSS''Z'''));
+t_start = t_start - 5*60*60;
+t_end = posixtime(datetime(end_time, 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss.SSSSSS''Z'''));
+t_end = t_end - 5*60*60;
+ts = (t_end - t_start) / numframes;
+
+timestamps = (t_start : ts : t_end-ts).';
 
 a = squeeze(frames(1:numsamples, 1, 1, :));
 b = reshape(a.', chirplen, framelen, []);
@@ -28,19 +32,18 @@ u = framelen/2 + (-framelen_dec/2 : framelen_dec/2-1);
 v = 1:chirplen_dec;
 x = (u-1-framelen/2) * 0.0156;
 y = (v-1) * 0.04;
-c = fftshift(fft2(b), 2);
+c = permute(fftshift(fft2(b), 2), [3 1 2]);
 scans = abs(c);
 
 if doplot
     f = waitbar(0, 'Plotting frames');
     for i = 1:numframes
-        imcomplex(x, y, abs(c(v, u, i)));
+        imcomplex(x, y, abs(c(i, v, u)));
         f = waitbar(i/numframes, f, 'Plotting frames');
-        pause(duration / numframes);
+        pause(ts);
     end
     close(f);
 end
 
-timestamps = posixtime(dt_start) : duration/numframes : posixtime(dt_end);
 
 end
--- matlab/traj_from_file.m
+++ matlab/traj_from_file.m
@@ -1,26 +1,30 @@
 function [pos, rot, vel] = traj_from_file(filename, scan_t)
 
-%TODO interpolate from traj_t to scan_t
-%TODO compute velocity
+%TODO coordinate frame transform
 
 traj = jsondecode(fileread(filename));
 N = size(traj, 1);
 
-traj_t = zeros(N, 1);
-pos = zeros(N, 3);
-rot = zeros(N, 3, 3);
-vel = zeros(N, 3);
+waypoint_ts = zeros(N, 1);
+waypoints = zeros(N, 3);
+dirs = zeros(3, 3, N);
+
 f = waitbar(0, 'Loading trajectory');
 for p = 1:N
     pose = traj(p).data;
     dt = datetime(pose.timestamp, 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss.SSSSSS''Z''');
-    traj_t(p) = posixtime(dt);
-    pos(p, 1) = pose.position.x;
-    pos(p, 2) = pose.position.y;
-    pos(p, 3) = pose.position.z;
-    rot(p, :, :) = quat2rotm(quaternion(pose.rotation.w, pose.rotation.x, pose.rotation.y, pose.rotation.z));
+    waypoint_ts(p) = posixtime(dt);
+    waypoints(p, 1) = pose.position.x;
+    waypoints(p, 2) = pose.position.y;
+    waypoints(p, 3) = pose.position.z;
+    dirs(:, :, p) = quat2rotm(quaternion(pose.rotation.w, pose.rotation.x, pose.rotation.y, pose.rotation.z));
     f = waitbar(p/N, f, 'Loading trajectory');
 end
 close(f);
+
+traj = waypointTrajectory(waypoints, waypoint_ts, Orientation=dirs, ReferenceFrame='ENU');
+
+[pos, rot, vel, ~, ~] = traj.lookupPose(scan_t);
+rot = permute(quat2rotm(rot), [3 2 1]); % lookupPose provides world->body, so we need to invert
 
 end
