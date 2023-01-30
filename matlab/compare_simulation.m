5c09ca70c9d449fbd0fad7e434028384cb116c86
John <jmiller4@andrew.cmu.edu>
2023-01-30 14:03:17 -0500

    better matlab vis

--- matlab/compare_simulation.m
+++ matlab/compare_simulation.m
@@ -13,15 +13,19 @@ y = linspace(min_range, max_range, floor(res_range));
 N = size(rad, 1);
 ts = t(2)-t(1);
 for i = 1:N
-    c = squeeze(rad(i,:,:));
-    d = squeeze(sim(i,:,:));
-    f = squeeze(pred(i,:,:));
+    c = squeeze(rad(i,1:64,:));
+    d = squeeze(sim(i,1:64,:));
+%     f = squeeze(pred(i,:,:));
     c(:,33) = 0;
-    subplot(3,1,1);
+    d(:,33) = 0;
+    subplot(2,1,1);
     imcomplex(x, y, c);
-    subplot(3,1,2);
+    title('real');
+    subplot(2,1,2);
     imcomplex(x, y, d);
-    subplot(3,1,3);
-    imcomplex(x, y, f);
+    title('sim');
+%     subplot(3,1,3);
+%     imcomplex(x, y, f);
+%     title('pred');
     pause(ts);
 end
