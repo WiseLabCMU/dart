370c03498fa1b2ede017870133cfcc39a502418a
John <jmiller4@andrew.cmu.edu>
2023-02-14 15:47:23 -0500

    slider updates scan plots

--- matlab/compare_simulation.m
+++ matlab/compare_simulation.m
@@ -5,9 +5,8 @@ dataset = 'cubes';
 
 real_rad = rad;
 load(simfile, 'rad');
-sim = rad;
+sim_rad = rad;
 rad = real_rad;
-clear real_rad;
 
 x = linspace(min_doppler, max_doppler, res_doppler);
 y = linspace(min_range, max_range/2, floor(res_range/2));
@@ -27,32 +26,23 @@ quiver3(pos(:,1),pos(:,2),pos(:,3),v(:,1),v(:,2),v(:,3));
 patch('Faces',fo,'Vertices',vo,'FaceColor','#909090','EdgeColor','None');
 axis equal; axis vis3d;
 
-zeta = 0;
-sld = uicontrol('Parent',fig,'Style','slider','value',zeta,'min',0,'max',1,'Position',[1200,125,1200,50]);
-bl1 = uicontrol('Parent',fig,'Style','text','Position',[1150,135,23,30],'String','0','FontSize',16);
-bl2 = uicontrol('Parent',fig,'Style','text','Position',[2420,135,23,30],'String','1','FontSize',16);
-bl3 = uicontrol('Parent',fig,'Style','text','Position',[1750,90,100,30],'String','Time','FontSize',16);
-
 N = size(rad, 1);
+sld = uicontrol('Parent',fig,'Style','slider','Value',1,'min',1,'max',N,'Position',[1200,125,1200,50],'SliderStep',[1,1]./(N-1));
+bl1 = uicontrol('Parent',fig,'Style','text','Position',[1150,135,23,30],'String','0','FontSize',16);
+tend = string(t(end)-t(1));
+bl2 = uicontrol('Parent',fig,'Style','text','Position',[2420,135,100,30],'String',tend,'FontSize',16);
+bl3 = uicontrol('Parent',fig,'Style','text','Position',[1750,90,100,30],'String','Time (s)','FontSize',16);
+
 ts = t(2)-t(1);
-for i = 1:N
-    c = squeeze(rad(i,1:64,:));
-    d = squeeze(sim(i,1:64,:));
-%     f = squeeze(pred(i,:,:));
+while true
+    sld.Value = round(sld.Value);
+    c = squeeze(real_rad(sld.Value,1:64,:));
+    d = squeeze(sim_rad(sld.Value,1:64,:));
     c(:,33) = 0;
     d(:,33) = 0;
-%     c = c - min(c, [], 2);
-%     c = c ./ max(c, [], 2);
-%     d = d - min(d, [], 2);
-%     d = d ./ max(d, [], 2);
     subplot(2,3,1);
     imcomplex(x, y, c);
-    title('real');
     subplot(2,3,4);
     imcomplex(x, y, d);
-    title('sim');
-%     subplot(3,1,3);
-%     imcomplex(x, y, f);
-%     title('pred');
     pause(ts);
 end
