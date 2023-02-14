380a6519f1844bfecba3ebe7b32317717094be2b
John <jmiller4@andrew.cmu.edu>
2023-02-14 13:17:40 -0500

    add slider

--- matlab/compare_simulation.m
+++ matlab/compare_simulation.m
@@ -12,6 +12,7 @@ clear real_rad;
 x = linspace(min_doppler, max_doppler, res_doppler);
 y = linspace(min_range, max_range/2, floor(res_range/2));
 
+fig = figure;
 subplot(2,3,[2,3,5,6]);
 facing = zeros(size(pos));
 for i = 1:size(facing, 1)
@@ -23,8 +24,14 @@ hold on
 v = max(min(vel,0.2),-0.2);
 quiver3(pos(:,1),pos(:,2),pos(:,3),v(:,1),v(:,2),v(:,3));
 [fo, vo] = isosurface(map.x,map.y,map.z,map.v);
-patch('Faces',fo,'Vertices',vo,'FaceColor','#7F7F7F','EdgeColor','None');
+patch('Faces',fo,'Vertices',vo,'FaceColor','#909090','EdgeColor','None');
 axis equal; axis vis3d;
+
+zeta = 0;
+sld = uicontrol('Parent',fig,'Style','slider','value',zeta,'min',0,'max',1,'Position',[1200,125,1200,50]);
+bl1 = uicontrol('Parent',fig,'Style','text','Position',[1150,135,23,30],'String','0','FontSize',16);
+bl2 = uicontrol('Parent',fig,'Style','text','Position',[2420,135,23,30],'String','1','FontSize',16);
+bl3 = uicontrol('Parent',fig,'Style','text','Position',[1750,90,100,30],'String','Time','FontSize',16);
 
 N = size(rad, 1);
 ts = t(2)-t(1);
