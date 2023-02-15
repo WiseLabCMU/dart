72009633c74ac88545490a620759f248862739d4
John <jmiller4@andrew.cmu.edu>
2023-02-15 13:07:24 -0500

    add axis labels

--- matlab/compare_simulation.m
+++ matlab/compare_simulation.m
@@ -26,6 +26,11 @@ light;
 cam = plotCamera('Size',0.1,'Opacity',0.1);
 axis equal; axis vis3d;
 axis([-2,3,-2,3,0,2]);
+xlabel('x (m)');
+ylabel('y (m)');
+zlabel('z (m)');
+title('Scene');
+set(gca, 'FontSize', 16);
 
 N = size(rad, 1);
 sld = uicontrol('Parent',fig,'Style','slider','Value',1,'Min',1,'Max',N,'Position',[1200,125,1200,50],'SliderStep',[1,1]./(N-1));
@@ -33,7 +38,7 @@ bl1 = uicontrol('Parent',fig,'Style','text','Position',[1150,135,23,30],'String'
 tend = string(t(end)-t(1));
 bl2 = uicontrol('Parent',fig,'Style','text','Position',[2420,135,100,30],'String',tend,'FontSize',16);
 bl3 = uicontrol('Parent',fig,'Style','text','Position',[1750,90,100,30],'String','Time (s)','FontSize',16);
-btn = uicontrol('Parent',fig,'Style','checkbox','Position',[1250,190,100,30],'String','Auto','Value',false,'FontSize',16);
+btn = uicontrol('Parent',fig,'Style','checkbox','Position',[1250,90,100,30],'String','Auto','Value',false,'FontSize',16);
 
 q1 = quiver3(0,0,0,0,0,0,'k', ...
     'XDataSource','pos(sld.Value,1)', ...
@@ -52,21 +57,32 @@ q2 = quiver3(0,0,0,0,0,0, ...
     'WDataSource','facing(sld.Value,3)', ...
     'LineWidth',2);
 
-
+fig.WindowState = 'maximized';
 ts = t(2)-t(1);
 while true
     if btn.Value
         sld.Value = min(sld.Value + 1, sld.Max);
     end
     sld.Value = round(sld.Value);
-    c = squeeze(real_rad(sld.Value,1:64,:));
-    d = squeeze(sim_rad(sld.Value,1:64,:));
-    c(:,33) = 0;
-    d(:,33) = 0;
+
     subplot(2,3,1);
+    c = squeeze(real_rad(sld.Value,1:64,:));
+    c(:,33) = 0;
     imcomplex(x, y, c);
+    xlabel('Doppler (m/s)');
+    ylabel('Range (m)');
+    title('Real Scans');
+    set(gca, 'FontSize', 16);
+
     subplot(2,3,4);
+    d = squeeze(sim_rad(sld.Value,1:64,:));
+    d(:,33) = 0;
     imcomplex(x, y, d);
+    xlabel('Doppler (m/s)');
+    ylabel('Range (m)');
+    title('Simulated Scans');
+    set(gca, 'FontSize', 16);
+
     r = squeeze(rot(sld.Value,:,:)) * axang2rotm([0,1,0,deg2rad(90)]);
     p = pos(sld.Value,:);
     pose = rigidtform3d(r, p);
