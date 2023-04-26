f4a8ab184261a73b4c701fd06328623739a3a43f
wavy <johnmiller@cmu.edu>
2023-04-26 11:43:18 -0400

    [update] add script to collect optitrack using new natnet client library

--- scripts/optitrack/optitrack.py
+++ scripts/optitrack/optitrack.py
@@ -1,74 +1,97 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+# coding: utf-8
+"""Command-line NatNet client application for testing.
 
-import tables as tb
-import pyrealsense2 as rs
+Copyright (c) 2017, Matthew Edwards.  This file is subject to the 3-clause BSD
+license, as found in the LICENSE file in the top-level directory of this
+distribution and at https://github.com/mje-nz/python_natnet/blob/master/LICENSE.
+No part of python_natnet, including this file, may be copied, modified,
+propagated, or distributed except according to the terms contained in the
+LICENSE file.
+"""
 
-TRAJ_FILENAME = '../data/traj.h5'
-TRAJ_BUFSIZE = 200
+from __future__ import print_function
 
+import argparse
+import time
 
-class Pose(tb.IsDescription):
-    t       = tb.Float64Col()
-    x       = tb.Float64Col()
-    y       = tb.Float64Col()
-    z       = tb.Float64Col()
-    vx      = tb.Float64Col()
-    vy      = tb.Float64Col()
-    vz      = tb.Float64Col()
-    qw      = tb.Float64Col()
-    qx      = tb.Float64Col()
-    qy      = tb.Float64Col()
-    qz      = tb.Float64Col()
+import attr
+
+import natnet
 
 
-def datacollect():
-    pipe = rs.pipeline()
-    cfg = rs.config()
-    cfg.disable_all_streams()
-    cfg.enable_stream(rs.stream.pose)
-    pipe.start(cfg)
-    with tb.open_file(TRAJ_FILENAME, mode='w', title='Trajectory file') as h5file:
-        traj_group = h5file.create_group('/', 'traj', 'Trajectory information')
-        pose_table = h5file.create_table(traj_group, 'pose', Pose, 'Pose data')
-        pose_count = 0
-        start_time = None
-        end_time = None
-        try:
-            while True:
-                frames = pipe.poll_for_frames()
-                if frames:
-                    frame = frames.get_pose_frame()
-                    if frame:
-                        end_time = frame.timestamp / 1000
-                        if not start_time:
-                            start_time = end_time
-                        pose = frame.get_pose_data()
-                        pose_table.row['t'] = frame.timestamp / 1000
-                        pose_table.row['x'] = pose.translation.x
-                        pose_table.row['y'] = pose.translation.y
-                        pose_table.row['z'] = pose.translation.z
-                        pose_table.row['vx'] = pose.velocity.x
-                        pose_table.row['vy'] = pose.velocity.y
-                        pose_table.row['vz'] = pose.velocity.z
-                        pose_table.row['qw'] = pose.rotation.w
-                        pose_table.row['qx'] = pose.rotation.x
-                        pose_table.row['qy'] = pose.rotation.y
-                        pose_table.row['qz'] = pose.rotation.z
-                        pose_table.row.append()
-                        pose_count += 1
-                        if pose_count >= TRAJ_BUFSIZE:
-                            print(f'Flushing {pose_count} poses.')
-                            print(f'Trajectory time: {end_time - start_time}s\n')
-                            pose_table.flush()
-                            pose_count = 0
-        except KeyboardInterrupt:
-            pipe.stop()
-            print(f'Interrupted.')
-            print(f'Flushing {pose_count} poses.')
-            print(f'Trajectory time: {end_time - start_time}s\n')
-            pose_table.flush()
-            pose_count = 0
+@attr.s
+class ClientApp(object):
+
+    _client = attr.ib()
+    _quiet = attr.ib()
+
+    _last_printed = attr.ib(0)
+
+    @classmethod
+    def connect(cls, server_name, rate, quiet):
+        if server_name == 'fake':
+            client = natnet.fakes.SingleFrameFakeClient.fake_connect(rate=rate)
+        else:
+            client = natnet.Client.connect(server_name)
+        if client is None:
+            return None
+        return cls(client, quiet)
+
+    def run(self):
+        if self._quiet:
+            self._client.set_callback(self.callback_quiet)
+        else:
+            self._client.set_callback(self.callback)
+        self._client.spin()
+
+    def callback(self, rigid_bodies, markers, timing):
+        """
+
+        :type rigid_bodies: list[RigidBody]
+        :type markers: list[LabelledMarker]
+        :type timing: TimestampAndLatency
+        """
+        print()
+        print('{:.1f}s: Received mocap frame'.format(timing.timestamp))
+        if rigid_bodies:
+            print('Rigid bodies:')
+            for b in rigid_bodies:
+                print('\t Id {}: ({: 5.2f}, {: 5.2f}, {: 5.2f}), ({: 5.2f}, {: 5.2f}, {: 5.2f}, {: 5.2f})'.format(
+                    b.id_, *(b.position + b.orientation)
+                ))
+        if markers:
+            print('Markers')
+            for m in markers:
+                print('\t Model {} marker {}: size {:.4f}mm, pos ({: 5.2f}, {: 5.2f}, {: 5.2f}), '.format(
+                    m.model_id, m.marker_id, 1000*m.size, *m.position
+                ))
+        print('\t Latency: {:.1f}ms (system {:.1f}ms, transit {:.1f}ms, processing {:.2f}ms)'.format(
+            1000*timing.latency, 1000*timing.system_latency, 1000*timing.transit_latency,
+            1000*timing.processing_latency
+        ))
+
+    def callback_quiet(self, *_):
+        if time.time() - self._last_printed > 1:
+            print('.')
+            self._last_printed = time.time()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--server', help='Will autodiscover if not supplied')
+    parser.add_argument('--fake', action='store_true',
+                        help='Produce fake data at `rate` instead of connecting to actual server')
+    parser.add_argument('--rate', type=float, default=10,
+                        help='Rate at which to produce fake data (Hz)')
+    parser.add_argument('--quiet', action='store_true')
+    args = parser.parse_args()
+
+    try:
+        app = ClientApp.connect('fake' if args.fake else args.server, args.rate, args.quiet)
+        app.run()
+    except natnet.DiscoveryError as e:
+        print('Error:', e)
+
 
 if __name__ == '__main__':
-    datacollect()
+    main()
