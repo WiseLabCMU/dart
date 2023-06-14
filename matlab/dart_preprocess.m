5fae314da075a0c9eea58082e77decebc540c45d
CalculatorSP <mike.handidate@gmail.com>
2023-06-14 12:04:01 -0400

    add conditional azimuth processing and save radar frames as float16

--- matlab/dart_preprocess.m
+++ matlab/dart_preprocess.m
@@ -9,10 +9,11 @@ FORCE_REPROCESS_TRAJ = true;
 INTERP_TRAJ = false;
 INTERP_TRAJ_FS = 200;
 
-range_decimation = 4;      % max_range=21m when range_decimation=1
-doppler_decimation = 1;    % max_velocity=2m/s when doppler_decimation=1
-framelen = 256;
-stride = 64;
+RANGE_DECIMATION = 4;      % max_range=21m when range_decimation=1
+DOPPLER_DECIMATION = 1;    % max_velocity=2m/s when doppler_decimation=1
+FRAMELEN = 256;
+STRIDE = 64;
+PROCESS_AZIMUTH = true;
 
 CHIRPLEN = 512;
 CHIRP_DT = 1e-3;
@@ -38,6 +39,12 @@ GLOBAL_TFORM = [ 1,  0,  0,  0;
                  0,  1,  0,  0;
                  0,  0,  0,  1];
 
+if PROCESS_AZIMUTH
+    GAIN = 'awr1843boost_az8';
+else
+    GAIN = 'awr1843boost';
+end
+
 % ---------------------------------------------------------- %
 
 t265file = fullfile(DATADIR, DATASET, 't265.h5');
@@ -50,17 +57,17 @@ jsonfile = fullfile(DATADIR, DATASET, append(DATASET, '.json'));
 mapfile = fullfile(DATADIR, DATASET, 'map.mat');
 dbgfile = fullfile(DATADIR, DATASET, 'dbg.mat');
 
-bin_doppler = DMAX / framelen;
-res_doppler = framelen / doppler_decimation;
+bin_doppler = DMAX / FRAMELEN;
+res_doppler = FRAMELEN / DOPPLER_DECIMATION;
 min_doppler = -bin_doppler * (res_doppler * 0.5);
 max_doppler = bin_doppler * (res_doppler * 0.5 - 1);
 
 bin_range = RMAX / CHIRPLEN;
-res_range = CHIRPLEN / range_decimation;
+res_range = CHIRPLEN / RANGE_DECIMATION;
 min_range = bin_range * 0.5;
 max_range = bin_range * (res_range + 0.5);
 
-scan_window = CHIRP_DT * framelen;
+scan_window = CHIRP_DT * FRAMELEN;
 
 radarjson = struct();
 radarjson.theta_lim = deg2rad(90) - 0.001;
@@ -69,6 +76,7 @@ radarjson.n = 512;
 radarjson.k = 256;
 radarjson.r = [min_range, max_range, res_range];
 radarjson.d = [min_doppler, max_doppler, res_doppler];
+radarjson.gain = GAIN;
 jsonstring = jsonencode(radarjson, 'PrettyPrint', true);
 writelines(jsonstring, jsonfile);
 
@@ -100,10 +108,11 @@ for b = 0 : nbatches - 1
     fprintf('Batch %d/%d\n', b + 1, nbatches);
     [new_scan_t, new_rad] = timed_scans_from_file( ...
         scanfile, ...
-        range_decimation, ...
-        doppler_decimation, ...
-        framelen, ...
-        stride, ...
+        RANGE_DECIMATION, ...
+        DOPPLER_DECIMATION, ...
+        FRAMELEN, ...
+        STRIDE, ...
+        PROCESS_AZIMUTH, ...
         b * BATCH_SIZE + 1, ...
         min(BATCH_SIZE, nrows - b * BATCH_SIZE) ...
     );
@@ -124,11 +133,16 @@ t = scan_t;
 
 naan = isnan(pos(:,1));
 t(naan) = [];
-rad(naan, :, :, :) = [];
-% rad(naan, :, :) = [];
+if PROCESS_AZIMUTH
+    rad(naan, :, :, :) = [];
+else
+    rad(naan, :, :) = [];
+end
 pos(naan, :) = [];
 rot(naan, :, :) = [];
 vel(naan, :) = [];
+
+rad = half(rad);
 
 save(outfile, 't', 'rad', 'pos', 'rot', 'vel', '-v7.3');
 save(dbgfile, '-v7.3');
--- matlab/timed_scans_from_file.m
+++ matlab/timed_scans_from_file.m
@@ -4,6 +4,7 @@ function [timestamps, scans] = timed_scans_from_file( ...
     doppler_decimation, ...
     framelen, ...
     stride, ...
+    process_azimuth, ...
     start_row, ...
     num_rows ...
 )
@@ -25,36 +26,47 @@ w_r = hann(chirplen);
 w = w_r * w_d;
 
 timestamps = zeros(numframes, 1);
-scans = zeros(chirplen, framelen, 8, numframes);
-% scans = zeros(chirplen, framelen, numframes);
+if process_azimuth
+    scans = zeros(chirplen, framelen, 8, numframes);
+else
+    scans = zeros(chirplen, framelen, numframes);
+end
 for i = 1:numframes
     startchirp = stride * (i - 1) + 1;
     chirp_idx = startchirp : startchirp + framelen - 1;
     timestamps(i) = mean(frametimes(chirp_idx));
-    scans(:, :, 1, i) = double(squeeze(frames(chirp_idx, 1, 1, :))).' .* w;
-    scans(:, :, 2, i) = double(squeeze(frames(chirp_idx, 1, 2, :))).' .* w;
-    scans(:, :, 3, i) = double(squeeze(frames(chirp_idx, 1, 3, :))).' .* w;
-    scans(:, :, 4, i) = double(squeeze(frames(chirp_idx, 1, 4, :))).' .* w;
-    scans(:, :, 5, i) = double(squeeze(frames(chirp_idx, 3, 1, :))).' .* w;
-    scans(:, :, 6, i) = double(squeeze(frames(chirp_idx, 3, 2, :))).' .* w;
-    scans(:, :, 7, i) = double(squeeze(frames(chirp_idx, 3, 3, :))).' .* w;
-    scans(:, :, 8, i) = double(squeeze(frames(chirp_idx, 3, 4, :))).' .* w;
-    % scans(:, :, i) = double(squeeze(frames(chirp_idx, 1, 1, :))).' .* w;
+    if process_azimuth
+        scans(:, :, 1, i) = double(squeeze(frames(chirp_idx, 1, 1, :))).' .* w;
+        scans(:, :, 2, i) = double(squeeze(frames(chirp_idx, 1, 2, :))).' .* w;
+        scans(:, :, 3, i) = double(squeeze(frames(chirp_idx, 1, 3, :))).' .* w;
+        scans(:, :, 4, i) = double(squeeze(frames(chirp_idx, 1, 4, :))).' .* w;
+        scans(:, :, 5, i) = double(squeeze(frames(chirp_idx, 3, 1, :))).' .* w;
+        scans(:, :, 6, i) = double(squeeze(frames(chirp_idx, 3, 2, :))).' .* w;
+        scans(:, :, 7, i) = double(squeeze(frames(chirp_idx, 3, 3, :))).' .* w;
+        scans(:, :, 8, i) = double(squeeze(frames(chirp_idx, 3, 4, :))).' .* w;
+    else
+        scans(:, :, i) = double(squeeze(frames(chirp_idx, 1, 1, :))).' .* w;
+    end
 end
 
 scans = fft2(scans); % range-doppler
-scans = fft(scans, [], 3); % azimuth
-scans(:, [1:2,end], :, :) = scans(:, [1:2,end], :, :) - median(scans(:, [1:2,end], :, :), 4);
-% scans(:, [1:2,end], :) = scans(:, [1:2,end], :) - median(scans(:, [1:2,end], :), 3);
+if process_azimuth
+    scans = fft(scans, [], 3); % azimuth
+    scans(:, [1:2,end], :, :) = scans(:, [1:2,end], :, :) - median(scans(:, [1:2,end], :, :), 4);
+else
+    scans(:, [1:2,end], :) = scans(:, [1:2,end], :) - median(scans(:, [1:2,end], :), 3);
+end
 
 res_doppler = framelen / doppler_decimation;
 res_range = chirplen / range_decimation;
 scans = circshift(scans, floor(res_doppler / 2), 2);
-scans = circshift(scans, 4, 3);
-scans = permute(scans, [4 1 2 3]);
-% scans = permute(scans, [3 1 2]);
-
-scans = abs(scans(:, 1:res_range, 1:res_doppler, :));
-% scans = abs(scans(:, 1:res_range, 1:res_doppler));
+if process_azimuth
+    scans = circshift(scans, 4, 3);
+    scans = permute(scans, [4 1 2 3]);
+    scans = abs(scans(:, 1:res_range, 1:res_doppler, :));
+else
+    scans = permute(scans, [3 1 2]);
+    scans = abs(scans(:, 1:res_range, 1:res_doppler));
+end
 
 end
