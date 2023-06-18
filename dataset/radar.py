45afe4b8307b4329dd7f91fc4338a4a605ddc60d
Tianshu Huang <thetianshuhuang@gmail.com>
2023-06-18 01:35:30 -0400

    More work on refactor

--- dataset/radar.py
+++ dataset/radar.py
@@ -1,4 +1,4 @@
-"""Radar processing functions."""
+"""Radar processing routines."""
 
 from functools import partial
 from tqdm import tqdm as default_tqdm
@@ -8,55 +8,8 @@ from numpy.lib.stride_tricks import sliding_window_view
 from jax import numpy as jnp
 import jax
 
+from beartype.typing import NamedTuple
 from jaxtyping import Complex64, Array, Float64, Float32, Float16
-
-
-def range_doppler_azimuth(
-    frames: Complex64[Array, "frame antenna range doppler"],
-    max_range: int = 128
-) -> Float32[Array, "frame antenna range doppler"]:
-    """Process range-doppler-azimuth images.
-
-    Parameters
-    ----------
-    frames: radar frames to process, with axis order
-        [frame #, antenna #, sample #, chirp #]
-    max_range: number of range bins to keep.
-
-    Returns
-    -------
-    Range-doppler-azimuth array (magnitude only).
-    """
-    # Hanning window to suppress range, doppler bleed
-    window = (
-        jnp.hanning(frames.shape[3]).reshape(1, -1)
-        * jnp.hanning(frames.shape[2]).reshape(-1, 1))
-
-    # Range-doppler: fft along frame (-> doppler) and chirp (-> range)
-    rd = jnp.fft.fft2(frames * window.reshape(1, 1, *window.shape))
-    # Azimuth: fft along antenna
-    rda = jnp.fft.fft(rd, axis=1)
-
-    # fftshift along antenna, doppler axes
-    rda = jnp.fft.fftshift(rda, axes=[1, 3])[..., :max_range, :]
-
-    return jnp.abs(rda)
-
-
-def remove_artifact(
-    images: Float32[Array, "frame antenna range doppler"]
-) -> Float16[Array, "frame antenna range doppler"]:
-    """Remove zero-doppler artifact from images.
-
-    Collected range-doppler radar data will have an artifact at close ranges
-    in the zero-doppler column which corresponds to the radar return of the
-    data collection rig. We subtract the minimum observed value from each range
-    to get rid of this.
-    """
-    zero = int(images.shape[3] / 2)
-    artifact = jnp.min(images[..., zero - 1:zero + 2], axis=0)
-    return images.at[..., zero - 1:zero + 2].set(
-        images[..., zero - 1:zero + 2] - artifact.reshape(1, *artifact.shape))
 
 
 def to_float16(
@@ -65,87 +18,157 @@ def to_float16(
     """Convert array to float16.
 
     The largest observed value is scaled to the largest normal normal number
-    (65504) for 16-bit IEEE floats. The effective scale factor applied
-    is also returned.
+    (65504) for 16-bit IEEE floats.
     """
     maxval = np.max(arr)
-    return (arr / maxval * max_normal).astype(np.float16), maxval / max_normal
+    return (arr / maxval * max_normal).astype(np.float16)
 
 
-def calculate_intrinsics(
-    framelen: int = 256, max_range: int = 128, chirplen: int = 512,
-    dmax: float = 1.8949, rmax: float = 21.5991
-) -> dict:
-    """Get radar intrinsics."""
-    bin_doppler = dmax / framelen
-    bin_range = rmax / chirplen
-    return {
-        "gain": "awr1843boost_az8",
-        "n": 512, "d": 256,
-        "r": [
-            bin_range * 0.5,
-            bin_range * (min(max_range, chirplen) + 0.5),
-            min(max_range, chirplen)
-        ],
-        "d": [
-            -bin_doppler * framelen * 0.5,
-            bin_doppler * (framelen * 0.5 - 1),
-            framelen
-        ]
-    }
+class AWR1843Boost(NamedTuple):
+    """TI AWR1843Boost Radar Configuration.
 
-
-def process_data(
-    chirps: Complex64[np.ndarray, "frame tx rx chirp"],
-    timestamps: Float64[np.ndarray, "frame"],
-    max_range: int = 128, framelen: int = 256, stride: int = 64,
-    batch_size: int = 64, tqdm=default_tqdm
-) -> tuple[
-    Float16[Array, "idx antenna range doppler"],
-    Float64[Array, "idx"], dict
-]:
-    """Process radar data.
-
-    Parameters
+    Attributes
     ----------
-    chirps: Raw chirp data. The number of samples per chirp corresponds to
-        the maximum number of range bins available.
-    timestamps: timestamps of each frame.
-    max_range: Maximum range bin to keep (i.e. range decimation).
     framelen: Length of each frame; processed as a rolling frame. Corresponds
         to the number of doppler bins available.
-    stride: Stride between successive frames for rolling frame processing.
-    batch_size: Frame processing batch. 32/64/128 seems to be the fastest based
-        on some rough experiments on a RTX 4090 and 7950X.
-    tqdm: Progress bar class.
-
-    Returns
-    -------
-    [0] processed azimuth-range-doppler images.
-    [1] timestamps for each output.
-    [2] radar intrinsics for this dataset.
+    max_range: Maximum range bin to keep (i.e. range decimation).
+    chirplen: Number of samples in each chirp.
+    chirploops: Number of chirps looped per frame.
+    dmax: Doppler bandwidth.
+    rmax: Radar max range.
+    num_rx: Number of rx antenna.
+    num_tx: Number of tx antenna.
+    artifact_threshold: Zero-doppler artifact clipping thershold (in percent).
     """
-    # Discard the middle TX antenna, and flatten the antennas.
-    chirps_flat = chirps[:, (0, 2)].reshape(-1, 8, chirps.shape[3])
 
-    # Sliding window over chirps using stride_tricks; the window contents axis
-    # (frame -> doppler) is put in the last axis.
-    frames = sliding_window_view(
-        chirps_flat, window_shape=framelen, axis=0)[::stride]
-    image_times = np.mean(
-        sliding_window_view(timestamps, window_shape=framelen)[::stride],
-        axis=1)
+    framelen: int = 256
+    max_range: int = 128
+    chirplen: int = 512
+    chirploops: int = 1
+    dmax: float = 1.8949
+    rmax: float = 21.5991
+    num_rx: int = 4
+    num_tx: int = 3
+    artifact_threshold: float = 25.0
 
-    process_func = jax.jit(partial(range_doppler_azimuth, max_range=max_range))
+    @property
+    def frame_size(self):
+        """Number of complex entries in each frame."""
+        return self.chirploops * self.num_rx * self.num_tx * self.chirplen
 
-    res = []
-    for _ in tqdm(range(int(np.ceil(frames.shape[0] / batch_size)))):
-        res.append(process_func(jnp.array(frames[:batch_size])))
-        frames = frames[batch_size:]
+    @property
+    def frame_shape(self):
+        """Frame shape."""
+        return (self.chirploops * self.num_tx, self.num_rx, self.chirplen)
 
-    res, scale = to_float16(remove_artifact(jnp.concatenate(res, axis=0)))
-    cfg = calculate_intrinsics(
-        framelen=framelen, max_range=max_range, chirplen=chirps.shape[3])
-    cfg["scale"] = float(scale)
 
-    return res, image_times, cfg
+    def range_doppler_azimuth(
+        self, frames: Complex64[Array, "frame antenna range doppler"],
+    ) -> Float32[Array, "frame antenna range doppler"]:
+        """Process range-doppler-azimuth images.
+
+        Parameters
+        ----------
+        frames: radar frames to process, with axis order
+            [frame #, antenna #, sample #, chirp #]
+
+        Returns
+        -------
+        Range-doppler-azimuth array (magnitude only).
+        """
+        # Hanning window to suppress range, doppler bleed
+        window = (
+            jnp.hanning(frames.shape[3]).reshape(1, -1)
+            * jnp.hanning(frames.shape[2]).reshape(-1, 1))
+
+        # Range-doppler: fft along frame (-> doppler) and chirp (-> range)
+        rd = jnp.fft.fft2(frames * window.reshape(1, 1, *window.shape))
+        # Azimuth: fft along antenna
+        rda = jnp.fft.fft(rd, axis=1)
+
+        # fftshift along antenna, doppler axes
+        rda = jnp.fft.fftshift(rda, axes=[1, 3])[..., :self.max_range, :]
+
+        return jnp.abs(rda)
+
+    def remove_artifact(
+        self, images: Float32[Array, "frame antenna range doppler"],
+    ) -> Float16[Array, "frame antenna range doppler"]:
+        """Remove zero-doppler artifact from images.
+
+        Collected range-doppler radar data will have an artifact at close
+        ranges in the zero-doppler column which corresponds to the radar return
+        of the data collection rig. We subtract the `threshold` percentile
+        value from each range bin to get rid of this.
+        """
+        zero = int(images.shape[3] / 2)
+        artifact = jnp.percentile(
+            images[..., zero - 1:zero + 2], self.artifact_threshold, axis=0)
+        return images.at[..., zero - 1:zero + 2].set(
+            images[..., zero - 1:zero + 2]
+            - artifact.reshape(1, *artifact.shape))
+
+    def process_data(
+        self, chirps: Complex64[np.ndarray, "frame tx rx chirp"],
+        timestamps: Float64[np.ndarray, "frame"], stride: int = 64,
+        batch_size: int = 64, tqdm=default_tqdm
+    ) -> tuple[
+        Float16[Array, "idx antenna range doppler"], Float64[Array, "idx"]
+    ]:
+        """Process radar data.
+
+        Parameters
+        ----------
+        chirps: Raw chirp data. The number of samples per chirp corresponds to
+            the maximum number of range bins available.
+        timestamps: timestamps of each frame.
+        stride: Stride between successive frames for rolling frame processing.
+        batch_size: Frame processing batch. 32/64/128 seems to be the fastest
+            based on some rough experiments on a RTX 4090 and 7950X.
+        tqdm: Progress bar class.
+
+        Returns
+        -------
+        [0] processed azimuth-range-doppler images.
+        [1] timestamps for each output.
+        """
+        # Discard the middle TX antenna, and flatten the antennas.
+        chirps_flat = chirps[:, (0, 2)].reshape(-1, 8, chirps.shape[3])
+
+        # Sliding window over chirps using stride_tricks; the window contents axis
+        # (frame -> doppler) is put in the last axis.
+        frames = sliding_window_view(
+            chirps_flat, window_shape=self.framelen, axis=0)[::stride]
+        image_times = np.mean(
+            sliding_window_view(
+                timestamps, window_shape=self.framelen)[::stride], axis=1)
+
+        process_func = jax.jit(partial(self.range_doppler_azimuth))
+
+        res = []
+        for _ in tqdm(range(int(np.ceil(frames.shape[0] / batch_size)))):
+            res.append(process_func(jnp.array(frames[:batch_size])))
+            frames = frames[batch_size:]
+
+        res = to_float16(self.remove_artifact(jnp.concatenate(res, axis=0)))
+
+        return res, image_times
+
+    def to_instrinsics(self) -> dict:
+        """Export intrinsics configuration file."""
+        bin_doppler = self.dmax / self.framelen
+        bin_range = self.rmax / self.chirplen
+        return {
+            "gain": "awr1843boost_az8",
+            "n": 512, "d": 256,
+            "r": [
+                bin_range * 0.5,
+                bin_range * (min(self.max_range, self.chirplen) + 0.5),
+                min(self.max_range, self.chirplen)
+            ],
+            "d": [
+                -bin_doppler * self.framelen * 0.5,
+                bin_doppler * (self.framelen * 0.5 - 1),
+                self.framelen
+            ]
+        }
---
+++ dataset/radar_dataset.py
@@ -0,0 +1,77 @@
+"""Radar Data Marshalling."""
+
+import numpy as np
+
+from beartype.typing import NamedTuple
+from jaxtyping import Complex64, Int16
+
+from .radar import AWR1843Boost
+
+
+class AWR1843BoostDataset(NamedTuple):
+    """Radar raw dataset."""
+
+    start: int
+    end: int
+    start_packet: int
+    frame_size: int
+    packet_size: int
+    num_packets: int
+
+    @classmethod
+    def from_packets(cls, packets, frame_size: int):
+        """Initialize from packet dictionary / h5file."""
+        packet_num = packets['packet_num']
+        num_packets = np.max(packet_num) - np.min(packet_num) + 1
+
+        byte_count = packets["byte_count"]
+        first_frame = np.ceil(byte_count[0] / 2 / frame_size).astype(int)
+        last_frame = np.floor(byte_count[-1] / 2 / frame_size).astype(int)
+        start = int(first_frame * frame_size - byte_count[0] / 2)
+        end = int(last_frame * frame_size - byte_count[0] / 2)
+
+        return cls(
+            start=start, end=end, start_packet=np.min(packet_num),
+            packet_size=packets["packet_data"].shape[1],
+            frame_size=frame_size, num_packets=num_packets)
+
+    def _get_valid(self, packets):
+        """Get valid frame mask."""
+        valid = np.zeros(self.num_packets, dtype=bool)
+        valid[packets["packet_num"] - self.start_packet] = True
+        valid = np.repeat(valid, self.packet_size)
+        valid_frames = np.all(
+            valid[self.start:self.end].reshape(-1, self.frame_size), axis=1)
+        return valid_frames
+
+    def _get_frames(self, packets, valid):
+        """Get frames."""
+        rad = np.zeros((self.num_packets, self.packet_size), dtype=np.int16)
+        rad[packets["packet_num"] - self.start_packet] = packets['packet_data']
+        rad = rad.reshape(-1)
+        return rad[self.start:self.end].reshape(-1, self.frame_size)[valid]
+
+    def _get_times(self, packets, valid):
+        """Get timestamps for each frame."""
+        start = np.arange(np.sum(valid)) * self.frame_size + self.start
+        frame_start_packet = np.floor(start / self.packet_size).astype(int)
+
+        timestamps = np.zeros(self.num_packets, dtype=np.float64)
+        timestamps[packets["packet_num"] - self.start_packet] = packets['t']
+        return timestamps[frame_start_packet]
+
+    def _to_iq(
+        self, radar: AWR1843Boost, frames: Int16[np.ndarray, "frames len"]
+    ) -> Complex64[np.ndarray, "frames tx rx chirp"]:
+        """Convert frames to a complex IQ array."""
+        iq = np.zeros((frames.shape[0], radar.frame_size), dtype=np.complex64)
+        iq[:, 0::2] = frames[:, 0::4] + 1j * frames[:, 2::4]
+        iq[:, 1::2] = frames[:, 1::4] + 1j * frames[:, 3::4]
+        return iq.reshape((-1, *radar.frame_shape))
+
+    def as_frames(self, radar, packets):
+        """Convert packets to frames."""
+        valid = self._get_valid(packets)
+        data = self._get_frames(packets, valid)
+        times = self._get_times(packets, valid)
+        return self._to_iq(radar, data), times
