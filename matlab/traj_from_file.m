33b9cb6c45281fac90f8730c099770774bb65d9b
john <jmiller4@andrew.cmu.edu>
2023-06-22 10:40:04 -0400

    progress bar

--- matlab/traj_from_file.m
+++ matlab/traj_from_file.m
@@ -28,12 +28,20 @@ pos = zeros(M, 3);
 rot = zeros(M, 3, 3);
 vel = zeros(M, 3);
 fprintf('Processing %s...\n', filename);
+f = waitbar(0, 'Processing trajectory');
 for i = 1:M
+    waitbar(i/M, f, 'Processing trajectory');
     w_pos = (scan_t(i)-scan_window/2) <= waypoint_t & waypoint_t <= (scan_t(i)+scan_window/2);
-    w_vel = (scan_t(i)-scan_window/2) <= waypoint_t & waypoint_t <= (scan_t(i)+scan_window/2);
+    w_vel = (scan_t(i)-scan_window/2) <= waypoint_t(1:end-1) & waypoint_t(1:end-1) <= (scan_t(i)+scan_window/2);
+    % Assume pose samples are equally spaced within the scan window
+    % h_pos = hann(sum(w_pos));
+    % h_vel = hann(sum(w_vel));
     pos(i, :) = mean(waypoint_pos(w_pos, :));
+    % pos(i, :) = sum(waypoint_pos(w_pos, :) .* h_pos) ./ sum(h_pos);
     rot(i, :, :) = quat2rotm(meanrot(waypoint_quat(w_pos, :)));
-    vel(i, :) = mean(waypoint_vel(w_vel(1:end-1), :));
+    vel(i, :) = mean(waypoint_vel(w_vel, :));
+    % vel(i, :) = sum(waypoint_vel(w_vel, :) .* h_vel) ./ sum(h_vel);
 end
+delete(f);
 
 end
