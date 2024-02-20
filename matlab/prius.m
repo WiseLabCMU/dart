2d174188e44e7a84629ead27e1ace37f6bcb2a42
Tianshu Huang <thetianshuhuang@gmail.com>
2024-02-20 17:07:15 -0500

    Clean up matlab scripts for release

--- matlab/prius.m
+++
@@ -1,8 +0,0 @@
-sigma = h5read('~/dart/results/cichallway_fixed/map.h5', '/sigma');
-alpha = h5read('~/dart/results/cichallway_fixed/map.h5', '/alpha');
-s = sigma;%(75:225,306:606,300:450);
-a = alpha;%(75:225,306:606,300:450);
-sss = (s-min(s(:)))./(max(s(:))-min(s(:)));
-aaa = (a-min(a(:)))./(max(a(:))-min(a(:)));
-
-vvv = volshow(1-10.^(-s));
